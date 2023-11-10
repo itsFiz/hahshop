be07a01d650ff260481b2cfc3bbc49fcbf9ed592
Harith <harith.nasahruddin10@gmail.com>
2023-11-10 11:03:50 +0800

    file management

--- back-end/src/main/java/com/ecommerce/main/Repositories/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.main.Repositories;
-
-import com.ecommerce.main.Model.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<User,Integer> {
-}
