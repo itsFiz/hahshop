2f4960fb4ecd41dbe249c8f943e2bd34d93dc136
Harith <harith.nasahruddin10@gmail.com>
2023-11-14 17:31:24 +0800

    view all seller frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,82 +1,43 @@
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
-import Topbar from "./Adminside/pages/global/TopBar";
-import Sidebar from "./Adminside/pages/global/SideBar";
-import Dashboard from "./Adminside/pages/dashboard/dashboard";
-import Category from "./Adminside/pages/category/category";
-import Bar from "./Adminside/pages/bar/bar";
-import Form from "./Adminside/pages/form/form";
-import Line from "./Adminside/pages/line/line";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-import { ColorModeContext, useMode } from "./theme";
-import Allorders from "./Adminside/pages/orders/Allorder";
-import Allproduct from "./Adminside/pages/product/Allproduct";
-import Allseller from "./Adminside/pages/seller/Allseller";
-// import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-import ProductCards from './glassmorphism/ProductCards'
-import UploadFile from './UploadFile'
-import NavBar from './components/NavBar'
-import ShoppingCart from './components/ShoppingCart'
-import Header from './NavbarComponent/Header'
-import UserLoginForm from "./UserComponent/UserLoginForm";
-import UserRegister from "./UserComponent/UserRegister";
-
-
+import ProductCards from './glassmorphism/ProductCards';
+import UploadFile from './UploadFile';
+import NavBar from './components/NavBar';
+import ShoppingCart from './components/ShoppingCart';
+import Header from './NavbarComponent/Header';
+import UserLogin from './UserModule/UserLogin';
 
 function App() {
-  const [theme, colorMode] = useMode();
-  const [isSidebar, setIsSidebar] = useState(true);
-
   return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <div className="app">
-          <Sidebar isSidebar={isSidebar} />
-          <main className="content">
-            <Topbar setIsSidebar={setIsSidebar} />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/category" element={<Category/>} />
-              <Route path="/allorder" element={<Allorders/>} />
-              <Route path="/allproduct" element={<Allproduct/>} />          
-              <Route path="/allseller" element={<Allseller/>} />          
-              <Route path="/form" element={<Form />} />
-              <Route path="/bar" element={<Bar />} />
-              <Route path="/line" element={<Line />} />
-              <Route path="/login" element={<UserLoginForm />} />
-              <Route path="/register-user" element={<UserRegister/>} />
-            </Routes>
-          </main>
-        </div>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
+    <Router>
+      <div>
+        <NavBar />
+        {/* Uncomment the components you want to render */}
+        {/* <ProductCards /> */}
+        {/* <UploadFile /> */}
+        {/* <ShoppingCart /> */}
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<LoginPage />} />
+        {/* USER PAGES */}
+        { <Route path="/register" element={<RegisterPage />} /> }
+        {/* <Route path="/productlisting" element={<ProductListingPage />} /> */}
+        {/* <Route path="/productdetails" element={<ProductDetailsPage />} /> */}
+        {/* <Route path="/addtocart" element={<CartPage />} /> */}
+        {/* <Route path="/checkoutpage" element={<CheckoutPage />} /> */}
+        {/* <Route path="/userprofile" element={<UserProfile />} /> */}
+        {/* SELLER PAGES */}
+        {/* <Route path="/sellerdashboard" element={<SellerDashboard />} /> */}
+        {/* <Route path="/productmanagement" element={<ProductManagement />} /> */}
+        {/* ADMIN PAGES */}
+        {/* <Route path="/adminportal" element={<AdminPortal />} /> */}
+        {/* <Route path="/adminportal" element={<Seller />} /> */}
+        {/* EXCEPTION PAGE */}
+        {/* <Route path="/exception" element={<Exception />} /> */}
+      </Routes>
+    </Router>
   );
 }
-export default App
 
-    // <Router>
-    //   <Routes>
-    //     <Route path="/" element={<Home />} />
-    //     <Route path="/login" element={<LoginPage />} />
-    //     <Route path="/register" element={<RegisterPage />} />
-    //     {/* USER PAGES */}
-    //     <Route path="/productlisting" element={<ProductListingPage />} />
-    //     <Route path="/productdetails" element={<ProductDetailsPage />} />
-    //     <Route path="/addtocart" element={<CartPage />} />
-    //     <Route path="/checkoutpage" element={<CheckoutPage />} />
-    //     <Route path="/userprofile" element={<UserProfile />} />
-    //     {/* SELLER PAGES */}
-    //     <Route path="/sellerdashboard" element={<SellerDashboard />} />
-    //     <Route path="/productmanagement" element={<ProductManagement />} />
-    //     <Route path="/productmanagement" element={<ProductManagement />} />
-    //     {/* ADMIN PAGES */}
-    //     <Route path="/adminportal" element={<AdminPortal />} />
-    //     <Route path="/adminportal" element={<Seller />} />
-
-    //     {/* EXCEPTION PAGE */}
-    //     <Route path="/exception" element={<Exception />} />
-
-    //   </Routes>
-    // </Router>
+export default App;
---
+++ front-end/src/UserComponent/ViewAllSellers.js
@@ -0,0 +1,187 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Paper,
+  Button,
+  Container,
+  Typography,
+  Snackbar,
+} from "@mui/material";
+import MuiAlert from "@mui/material/Alert";
+import { useNavigate } from "react-router-dom";
+
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+
+const ViewAllSellers = () => {
+  const [allSeller, setAllSeller] = useState([]);
+  const [snackbarOpen, setSnackbarOpen] = useState(false);
+  const [snackbarMessage, setSnackbarMessage] = useState("");
+  const [snackbarSeverity, setSnackbarSeverity] = useState("success");
+
+  const seller = JSON.parse(sessionStorage.getItem("active-seller"));
+  const admin_jwtToken = sessionStorage.getItem("admin-jwtToken");
+
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    const getAllUsers = async () => {
+      try {
+        const response = await retrieveAllUser();
+        if (response.success) {
+          setAllSeller(response.users);
+        } else {
+          handleSnackbar("Error: " + response.responseMessage, "error");
+        }
+      } catch (error) {
+        handleSnackbar("Error: Server is down", "error");
+      }
+    };
+
+    getAllUsers();
+  }, []);
+
+  const retrieveAllUser = async () => {
+    const response = await axios.get(
+      "http://localhost:8080/api/user/fetch/role-wise?role=Seller",
+      {
+        headers: {
+          Authorization: "Bearer " + admin_jwtToken,
+        },
+      }
+    );
+    return response.data;
+  };
+
+  const deleteSeller = (userId) => {
+    fetch("http://localhost:8080/api/user/delete/seller?sellerId=" + userId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + admin_jwtToken,
+      },
+    })
+      .then((result) => {
+        result.json().then((res) => {
+          if (res.success) {
+            handleSnackbar(res.responseMessage, "success");
+            setTimeout(() => {
+              window.location.reload(true);
+            }, 1000); // Reload after 1 second
+          } else if (!res.success) {
+            handleSnackbar(res.responseMessage, "error");
+          }
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        handleSnackbar("Error: Server is down", "error");
+      });
+  };
+
+  const handleSnackbar = (message, severity) => {
+    setSnackbarMessage(message);
+    setSnackbarSeverity(severity);
+    setSnackbarOpen(true);
+  };
+
+  const handleCloseSnackbar = () => {
+    setSnackbarOpen(false);
+  };
+
+  return (
+    <Container className="mt-3">
+      <Paper
+        elevation={3}
+        className="card form-card ms-2 me-2 mb-5 custom-bg shadow-lg"
+      >
+        <div
+          className="card-header custom-bg-text text-center bg-color"
+          style={{
+            borderRadius: "1em",
+            height: "50px",
+          }}
+        >
+          <Typography variant="h4">All Sellers</Typography>
+        </div>
+        <div
+          className="card-body"
+          style={{
+            overflowY: "auto",
+          }}
+        >
+          <TableContainer>
+            <Table>
+              <TableHead>
+                <TableRow>
+                  <TableCell>First Name</TableCell>
+                  <TableCell>Last Name</TableCell>
+                  <TableCell>Email Id</TableCell>
+                  <TableCell>Phone No</TableCell>
+                  <TableCell>Address</TableCell>
+                  <TableCell>Action</TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {allSeller.map((seller) => (
+                  <TableRow key={seller.id}>
+                    <TableCell>
+                      <b>{seller.firstName}</b>
+                    </TableCell>
+                    <TableCell>
+                      <b>{seller.lastName}</b>
+                    </TableCell>
+                    <TableCell>
+                      <b>{seller.emailId}</b>
+                    </TableCell>
+                    <TableCell>
+                      <b>{seller.phoneNo}</b>
+                    </TableCell>
+                    <TableCell>
+                      <b>
+                        {seller.address.street +
+                          ", " +
+                          seller.address.city +
+                          ", " +
+                          seller.address.pincode}
+                      </b>
+                    </TableCell>
+                    <TableCell>
+                      <Button
+                        onClick={() => deleteSeller(seller.id)}
+                        variant="outlined"
+                        color="error"
+                        size="small"
+                      >
+                        Delete
+                      </Button>
+                    </TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </div>
+      </Paper>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={6000}
+        onClose={handleCloseSnackbar}
+      >
+        <Alert onClose={handleCloseSnackbar} severity={snackbarSeverity}>
+          {snackbarMessage}
+        </Alert>
+      </Snackbar>
+    </Container>
+  );
+};
+
+export default ViewAllSellers;
